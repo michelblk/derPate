16c5c0649261bb757e1909fa970459524351ab3f
Michel Blank <michel.blank@deutschebahn.com>
2019-02-14 10:09:02 +0100

    Removed bug in TimeUtilTest

--- Workspace/DerPate/src/test/java/de/db/derPate/util/TimeUtilTest.java
+++ Workspace/DerPate/src/test/java/de/db/derPate/util/TimeUtilTest.java
@@ -1,5 +1,6 @@
 package de.db.derPate.util;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.text.DateFormat;
@@ -8,7 +9,7 @@ import java.util.Locale;
 
 import org.junit.Test;
 
-@SuppressWarnings({ "javadoc", "nls" })
+@SuppressWarnings({ "javadoc" })
 public class TimeUtilTest {
 	@Test
 	public void getCurrentTime() {
@@ -23,11 +24,12 @@ public class TimeUtilTest {
 	public void dateToReadableString() {
 		Locale locale = Locale.GERMANY;
 		Date date = new Date(1548694489000L);
-		String expected = "28.01.2019 17:54:49";
 
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT, locale);
-		String actual = dateFormat.format(date);
+		String expected = dateFormat.format(date);
 
-		// assertEquals(expected, actual);
+		String actual = TimeUtil.dateToReadableString(date, locale);
+
+		assertEquals(expected, actual);
 	}
 }
